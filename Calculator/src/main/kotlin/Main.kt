d47aa1688b3b2c6077b847d62d60bf16cd89965b
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-04-25 20:39:25 +0900

    추상 클래스 부분 수정,예외처리 기능 추가
    1. 오버플로우
    2. 오입력 대비
    3. 큰숫자에서 작은숫자를 0으로 나누는 대비
    4. 작은 숫자에서 큰 숫자 나누기

--- Calculator/src/main/kotlin/Main.kt
+++ Calculator/src/main/kotlin/Main.kt
@@ -4,22 +4,112 @@ fun main(args: Array<String>) {
 
     while (start() == "Y") {
 
-        println("연산할 첫번째 숫자를 입력해주세요")
-        val firstNumber = readLine()!!.toLong()
-        println("연산할 두번째 숫자를 입력해주세요")
-        val secondNumber = readLine()!!.toLong()
+        var checkFirstNumber = false
+        var firstNumber = 0L
+        while(!checkFirstNumber) {
+            println("연산할 첫번째 숫자를 입력해주세요")
+            val firstNumberInput = readln().toLongOrNull()
 
-        printCalculationGuideMessage()
-        val selectCalculationMethod = readLine()!!
-        when (selectCalculationMethod) {
-            "1" -> AddCalculator(firstNumber, secondNumber).Operation(firstNumber, secondNumber)
-            "2" -> SubstractCalculator(firstNumber, secondNumber).Operation(firstNumber, secondNumber)
-            "3" -> MultiplyCalculator(firstNumber, secondNumber).Operation(firstNumber, secondNumber)
-            "4" -> DivideCalculator(firstNumber, secondNumber).Operation(firstNumber, secondNumber)
-            "5" -> RemainderCalculator(firstNumber, secondNumber).Operation(firstNumber, secondNumber)
-            else -> reInputRequest()
-
+            if(firstNumberInput == null){
+                println("숫자를 입력 해주셔야 합니다.")
+            }else{
+                firstNumber = firstNumberInput
+                checkFirstNumber = true
+            }
         }
+
+        var checkSecondNumber = false
+        var secondNumber = 0L
+        while(!checkSecondNumber) {
+            println("연산할 두번째 숫자를 입력해주세요")
+            val secondNumberInput = readln().toLongOrNull()
+
+            if(secondNumberInput == null){
+                println("숫자를 입력 해주셔야 합니다.")
+            }else{
+                secondNumber = secondNumberInput
+                checkSecondNumber = true
+            }
+        }
+
+        if(firstNumber > secondNumber){
+            var checkSelectionMethod = false
+
+            while(!checkSelectionMethod) {
+
+                printCalculationGuideMessage()
+
+                val selectCalculationMethod = readln()
+
+                if(selectCalculationMethod == "1"){
+                    PlusOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else if(selectCalculationMethod == "2"){
+                    MinusOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else if(selectCalculationMethod == "3"){
+                    MultiplyOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else if(secondNumber == 0L && selectCalculationMethod == "4"){
+                    println("두번째 숫자가 0인 상태에서 나누기를 진행 할 수없습니다.")
+                    println("다시 입력해주세요!")
+                    println("-----------------------------------------------")
+                }else if(selectCalculationMethod == "4"){
+                    DivideOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else if(secondNumber == 0L && selectCalculationMethod == "5"){
+                    println("두번째 숫자가 0인 상태에서 나누기를 진행 할 수없습니다.")
+                    println("다시 입력해주세요!")
+                    println("-----------------------------------------------")
+                }else if(selectCalculationMethod == "5"){
+                    RemainderOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else{
+                    println("잘못된 입력입니다. 다시 선택해주세요")
+                    println("-------------------------------")
+                }
+            }
+        }else{
+            var checkSelectionMethod = false
+
+            while(!checkSelectionMethod) {
+
+                printCalculationGuideMessage()
+
+                val selectCalculationMethod = readln()
+
+                if(selectCalculationMethod == "1"){
+                    PlusOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else if(selectCalculationMethod == "2"){
+                    MinusOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else if(selectCalculationMethod == "3"){
+                    MultiplyOperation().operate(firstNumber, secondNumber)
+                    checkSelectionMethod = true
+                }else if(firstNumber == 0L && selectCalculationMethod == "4"){
+                    println("첫번째 숫자가 0인 상태에서 나누기를 진행 할 수없습니다.")
+                    println("다시 입력해주세요!")
+                    println("-----------------------------------------------")
+                }else if(selectCalculationMethod == "4"){
+                    println("이 계산기는 정수만 취급하기에 큰 숫자에서 작은 숫자를 나누면 0을 리턴합니다.")
+                    checkSelectionMethod = true
+                }else if(firstNumber == 0L && selectCalculationMethod == "5"){
+                    println("첫번째 숫자가 0인 상태에서 나누기를 진행 할 수없습니다.")
+                    println("다시 입력해주세요!")
+                    println("-----------------------------------------------")
+                }else if(selectCalculationMethod == "5"){
+                    println("이 계산기는 정수만 취급하기에 큰 숫자에서 작은 숫자를 나누면 0을 리턴합니다.")
+                    checkSelectionMethod = true
+                }else{
+                    println("잘못된 입력입니다. 다시 선택해주세요")
+                    println("-------------------------------")
+                }
+            }
+        }
+
+
+
 
     }
 }
@@ -34,10 +124,10 @@ fun start(): String {
     println("메뉴를 골라주세요")
     println("계속 계산을 원하시면 숫자 1")
     println("계산 종료를 원하시면 아무거나 입력해주세요")
-    val getStartRequest = readLine()!!
-    when (getStartRequest) {
-        "1" -> return "Y"
-        else -> return "N"
+    val getStartRequest = readln()
+    return when (getStartRequest) {
+        "1" -> "Y"
+        else -> "N"
     }
 }
 
@@ -48,43 +138,38 @@ fun printCalculationGuideMessage() {
     println("3. 곱하기")
     println("4. 나누기(몫 구하기)")
     println("5. 나누기(나머지 구하기)")
-    println("잘못 입력하시면 처음으로 넘어갑니다.")
 }
 
-fun reInputRequest() {
-    println("처음으로 돌아갑니다")
+abstract class AbstractOperation {
+    abstract fun operate(num1: Long, num2: Long)
 }
 
-abstract class AbstractOperation(num1: Long, num2: Long) {
-    abstract fun Operation(num1: Long, num2: Long)
-}
-
-
-class AddCalculator(num1: Long, num2: Long) : AbstractOperation(num1, num2) {
-    override fun Operation(num1: Long, num2: Long) {
-        println(num1 + num2)
+class PlusOperation : AbstractOperation() {
+    override fun operate(num1: Long, num2: Long) {
+        println("${num1} 더하기 ${num2}은(는) ${num1+num2}입니다." )
     }
 }
 
-class SubstractCalculator(num1: Long, num2: Long) : AbstractOperation(num1, num2) {
-    override fun Operation(num1: Long, num2: Long) {
-        println(num1 - num2)
-    }
-}
-class MultiplyCalculator(num1: Long, num2: Long) : AbstractOperation(num1, num2) {
-    override fun Operation(num1: Long, num2: Long) {
-        println(num1 * num2)
+class MinusOperation : AbstractOperation() {
+    override fun operate(num1: Long, num2: Long) {
+        println("${num1} 빼기 ${num2}은(는) ${num1-num2}입니다." )
     }
 }
 
-class DivideCalculator(num1: Long, num2: Long) : AbstractOperation(num1, num2) {
-    override fun Operation(num1:Long,num2: Long) {
-        println(num1 / num2)
+class MultiplyOperation : AbstractOperation() {
+    override fun operate(num1: Long, num2: Long) {
+        println("${num1} 곱하기 ${num2}은(는) ${num1-num2}입니다." )
     }
 }
 
-class RemainderCalculator(num1: Long, num2: Long) : AbstractOperation(num1, num2) {
-    override fun Operation(num1:Long,num2:Long) {
-        println(num1 % num2)
+class DivideOperation : AbstractOperation() {
+    override fun operate(num1:Long,num2: Long) {
+        println("${num1}를(을) ${num2}로(으로) 나누면 몫은 ${num1-num2} 입니다." )
+    }
+}
+
+class RemainderOperation : AbstractOperation() {
+    override fun operate(num1:Long,num2:Long) {
+        println("${num1}를(을) ${num2}로(으로) 나누면 나머지는 ${num1-num2} 입니다." )
     }
 }
